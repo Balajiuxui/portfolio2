848f442ea3c30c09b99f56cb20864c075a02e6f6
kwbalaji <your-email@example.com>
2025-07-01 21:58:10 +0530

    tranistiondonewiththreesection

--- src/script.js
+++ src/script.js
@@ -11,3 +11,15 @@ window.addEventListener('scroll', () => {
     lastScrollY = window.scrollY;
   });
 });
+
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+}, { threshold: 0.2 });
+
+reveals.forEach(reveal => observer.observe(reveal));
